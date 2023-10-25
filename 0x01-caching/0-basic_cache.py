c433c1bf5077dd64bf632dd04a4e5e3d73f723ea
RobertRuai <robertruai4@gmail.com>
2023-10-25 14:26:21 +0300

    0. Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,11 +10,9 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Must assign to the dictionary"""
-        if (key or item is not None):
+        if (key and item):
             self.cache_data[key] = item
 
     def get(self, key):
         """return value in self.cache_data linked to key"""
-        if (key is not None or key in self.cache_data):
-            return self.cache_data.get(key)
-        return None
+        return self.cache_data.get(key, None)
