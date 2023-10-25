4d24e5cf2ced30d656b98e9cfeadb8d3e5d43517
RobertRuai <robertruai4@gmail.com>
2023-10-25 14:19:52 +0300

    0. Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""basic_cache module"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic dictionary"""
+
+    def put(self, key, item):
+        """Must assign to the dictionary"""
+        if (key or item is not None):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return value in self.cache_data linked to key"""
+        if (key is not None or key in self.cache_data):
+            return self.cache_data.get(key)
