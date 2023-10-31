74ed7ca82270fa145300cb57dea67ab4fa66dacb
RobertRuai <robertruai4@gmail.com>
2023-10-31 16:52:31 +0300

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,11 +18,11 @@ class Config:
 
     app.config.from_object(Config)
 
-
     @babel.localeselector
     def get_locale():
-	    """get_locale func"""
+        """get_locale func"""
         return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/', methods=["GET"], strict_slashes=False)
 def hello():
