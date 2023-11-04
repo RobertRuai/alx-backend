4af1ae33245ba448673b7da4656af84fede93ed6
RobertRuai <robertruai4@gmail.com>
2023-11-04 14:47:28 +0300

    minor code logic edits

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -40,13 +40,12 @@ def hello():
 def get_locale():
     """ Gets the best matching language for user """
     lan = request.args.get('locale')
-    if lan is not None:
-        if lan in app.config['LANGUAGES']:
-            return lan
-        elif g.users['locale'] in app.config['LANGUAGES']:
-            return g.users['locale']
-        else:
-            return request.accept_languages.best_match(Config.LANGUAGES)
+    if lan and lan in app.config['LANGUAGES']:
+        return lan
+    elif flask.g.users['locale'] in app.config['LANGUAGES']:
+        return flask.g.users['locale']
+    else:
+        return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 def get_user(user_id):
